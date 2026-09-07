c39d8103496af5ae52c05862b322835231a3eb09
Teye Jeff <teyejeff18@gmail.com>
2026-09-07 19:43:56 +0000

    add

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send('CSE341 Web Services API');
+});
+
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
