acb1bdda9a7af13baba4331549c75d1de757ce0c
Teye Jeff <teyejeff18@gmail.com>
2026-09-17 07:13:36 +0000

    fix dynamic hosting for swagger ui

--- swagger.js
+++ swagger.js
@@ -1,17 +1,37 @@
-const swaggerAutogen = require('swagger-autogen')();
+const express = require('express');
+const app = express();
+const mongodb = require('./db/connect');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
-const doc = {
-  info: {
-    title: 'Contacts API',
-    description: 'Contacts REST API Documentation'
-  },
-  host: process.env.HOST || 'localhost:8080',
-  schemes: ['http', 'https']
-};
+const port = process.env.PORT || 8080;
 
-const outputFile = './swagger.json';
-const routesFiles = ['./routes/index.js']; // Point this to your main router file (e.g., ./routes/index.js or ./routes/contacts.js)
+app
+  .use(express.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  })
+  .use('/api-docs', (req, res, next) => {
+    // Dynamically override host and schemes based on incoming request headers
+    swaggerDocument.host = req.get('host');
+    swaggerDocument.schemes = [req.protocol];
+    req.swaggerDoc = swaggerDocument;
+    next();
+  }, swaggerUi.serve, swaggerUi.setup())
+  .use('/', require('./routes'));
 
-swaggerAutogen(outputFile, routesFiles, doc).then(() => {
-  require('./server.js'); // Automatically launches server after generation completes
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Connected to DB and listening on port ${port}`);
+    });
+  }
 });
